755d0ce03ec56b4f293da1b98aad7b064e367761
Muhammed Anas <muhammedanas4140@gmail.com>
2018-01-08 23:12:25 +0530

    Routes for Apis

---
+++ routes/api.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/users', (req, res) => {
+    res.send({method: 'GET'})
+})
+
+router.post('/user', (req, res) => {
+    res.send({method: 'POST'})
+})
+
+router.put('/user/:id', (req, res) => {
+    res.send({method: 'PUT'})
+})
+
+router.delete('/users', (req, res) => {
+    res.send({method: 'DELETE'})
+})
+module.exports = router;
